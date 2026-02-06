05eb8d8dfc5b99619f5c64f82f6ec55fadb1292f
Cuncunco <victouros@gmail.com>
2026-02-06 18:04:29 -0300

    first insomnia moves

---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: 'John Doe',
+            email: 'johndoe@gmail.com',
+            avatarUrl: 'https://github.com/diego3g.png'
+        }
+    })
+
+    const pool = await prisma.pool.create({
+        data: {
+            title: 'Example pool',
+            code: 'BOL123',
+            ownerId: user.id,
+
+            participants: {
+                create: {
+                    userId: user.id
+                }
+            }
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date:'2026-02-06T11:55:21.949Z',
+            firstTeamCountryCode: 'DE',
+            secondTeamCountryCode: 'BR',
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2026-02-06T13:55:21.949Z',
+            firstTeamCountryCode:'BR',
+            secondTeamCountryCode: 'AR',
+
+            guesses: {
+                create: {
+                    firstTeamPoints: 2,
+                    secondTeamPoints: 1,
+
+                    participant: {
+                        connect: {
+                            userId_poolId: {
+                                userId: user.id,
+                                poolId: pool.id,
+                            }
+                        }
+                    }
+                }
+            }
+        },
+    })
+}
+
+main()
